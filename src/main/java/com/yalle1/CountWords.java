a953d560f753c8e41027a37024ea4281845c047b
Yalle1 <ensaryalap1@outlook.com>
2024-03-13 00:54:37 +0100

    CountWords class completed, question 2/2

---
+++ src/main/java/com/yalle1/CountWords.java
@@ -0,0 +1,25 @@
+package com.yalle1;
+
+import java.util.Optional;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+
+        wordCount("Hello boys, how are we doing!?");
+
+    }
+
+    public static void wordCount(String sentence){
+
+        sentence = sentence.trim();
+
+        int count = 0;
+        for (String s : sentence.split(" ")) {
+            count++;
+        }
+
+        System.out.println("There are "+count+" words in the sentence");
+
+    }
+}

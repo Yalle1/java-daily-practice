90e2e778de0904ebe7444276716e7049cd9b61d1
Yalle1 <ensaryalap1@outlook.com>
2024-03-13 00:49:34 +0100

    IsPalindrome class completed, question 1/2

---
+++ src/main/java/com/yalle1/IsPalindrome.java
@@ -0,0 +1,35 @@
+package com.yalle1;
+
+public class IsPalindrome {
+
+
+    public static void main(String[] args) {
+
+        boolean b1 = isPalindrome("Anna");
+        boolean b2 = isPalindrome("Hello");
+        boolean b3 = isPalindrome("non");
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+
+
+
+    }
+
+    public static boolean isPalindrome(String str){
+
+        str = str.trim();
+
+        String reversed = "";
+
+        for (int i = str.length()-1; i >= 0; i--){
+
+            reversed += str.charAt(i);
+
+        }
+
+        return reversed.equalsIgnoreCase(str);
+
+    }
+}
